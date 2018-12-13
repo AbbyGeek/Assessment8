faa3be39a717f9983da50419e2ce417b63ce2802
Abby Wessels <abbywessels@quickenloans.com>
2018-12-13 16:48:21 -0500

    First Commit. Redid assessment so it makes more sense

---
+++ Assessment7/Controllers/ShopController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Assessment7.Models;
+
+namespace Assessment7.Controllers
+{
+    public class ShopController : Controller
+    {
+        // GET: Shop
+        public ActionResult Index()
+        {
+            ShopEntities ORM = new ShopEntities();
+
+            ViewBag.Inventory = ORM.Inventories.ToList();
+
+            return View();
+        }
+
+        public ActionResult AddItem()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddItem(Inventory Item)
+        {
+            ShopEntities ORM = new ShopEntities();
+            ORM.Inventories.Add(Item);
+            ORM.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult EditItem(int id)
+        {
+            ShopEntities ORM = new ShopEntities();
+            Inventory found = ORM.Inventories.Find(id);
+            return View(found);
+        }
+
+        public ActionResult SaveItem(Inventory updateItem)
+        {
+            ShopEntities ORM = new ShopEntities();
+            Inventory oldItem = ORM.Inventories.Find(updateItem.ItemID);
+
+            oldItem.ItemID = updateItem.ItemID;
+            oldItem.Name = updateItem.Name;
+            oldItem.Description = updateItem.Description;
+            oldItem.Quantity = updateItem.Quantity;
+            oldItem.Price = updateItem.Price;
+
+            ORM.Entry(oldItem).State = System.Data.Entity.EntityState.Modified;
+            ORM.SaveChanges();
+
+            return RedirectToAction("Index");
+
+        }
+        public ActionResult DeleteItem(int id)
+        {
+            ShopEntities ORM = new ShopEntities();
+            Inventory found = ORM.Inventories.Find(id);
+            ORM.Inventories.Remove(found);
+            ORM.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
